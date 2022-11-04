ff9a42abcb2f5782b51a9db76ecb6772d76db7df
Oliverrod09 <oliver.antionio.09@gmail.com>
2022-11-04 00:10:03 -0600

    transicion de menu

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 var hamburguer = document.querySelector(".hamburguer");
 var menu = document.querySelector(".menuBtn");
+var lineaUno = document.querySelector(".line--1");
+var lineaDos = document.querySelector(".line--2");
+var lineaTres = document.querySelector(".line--3");
 
 hamburguer.addEventListener("click", ()=>{
     menu.classList.toggle('mostrarMenu');
+    lineaUno.classList.toggle("activeline--1");
+    lineaDos.classList.toggle("activeline--2");
+    lineaTres.classList.toggle("activeline--3");
 });
